ce5936cbe2300643619118a0bea7ae440284e7a4
Muhammad Ali <aliawan3032@gmail.com>
2024-02-07 02:02:19 +0500

    Changes

--- home/views.py
+++ home/views.py
@@ -5,7 +5,17 @@ from django.shortcuts import render
 from django.http import HttpResponse   # our backend server will return a http respnse
 
 def home(request):
-    return render(request , "index.html")
+
+    people_data = [
+        {'name : leonardo' ,'age: 55'},
+        {'name : Robert' ,'age: 45'},
+        {'name : Qasim' ,'age: 20'},
+        {'name : Ali' , 'age: 21'}
+    #we can pass data from backend to HTML file through context
+    ]
+    for people in people_data:
+        print(people)
+    return render(request , "index.html" , context ={'people_data':people_data})
 
 #when some1 will call main route then our home func shoul be triggered
 
